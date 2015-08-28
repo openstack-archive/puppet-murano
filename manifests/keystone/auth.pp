de0c10deacb0f13e721bd5742bf13429eca575bc
Alexey Deryugin <aderyugin@mirantis.com>
2015-08-28 15:09:00 +0300

    Keystone auth class implementation
    Change-Id: I065ed692b568ec534e52691ab43fdc6b9c861f8b

---
+++ manifests/keystone/auth.pp
@@ -0,0 +1,93 @@
+# == Class: murano::keystone::auth
+#
+# Configures murano service and endpoint in Keystone.
+#
+# === Parameters
+#
+# [*password*]
+#   (required) Password for murano user.
+#
+# [*service_name*]
+#   (Optional) Name of the service.
+#   Defaults to the value of auth_name.
+#
+# [*auth_name*]
+#   (Optional) Username for murano service.
+#   Defaults to 'murano'.
+#
+# [*email*]
+#   (Optional) Email for murano user.
+#   Defaults to 'murano@localhost'.
+#
+# [*tenant*]
+#   (Optional) Tenant for murano user.
+#   Defaults to 'services'.
+#
+# [*configure_endpoint*]
+#   (Optional) Should murano endpoint be configured?
+#   Defaults to 'true'.
+#
+# [*service_type*]
+#   (Optional) Type of service.
+#   Defaults to 'application-catalog'.
+#
+# [*service_description*]
+#   (Optional) Description of service.
+#   Defaults to 'Murano Application Catalog'.
+#
+# [*region*]
+#   (Optional) Region for endpoint.
+#   Defaults to 'RegionOne'.
+#
+# [*public_url*]
+#   (optional) The endpoint's public url. (Defaults to 'http://127.0.0.1:8082
+#   This url should *not* contain any trailing '/'.
+#
+# [*admin_url*]
+#   (optional) The endpoint's admin url. (Defaults to 'http://127.0.0.1:8082
+#   This url should *not* contain any trailing '/'.
+#
+# [*internal_url*]
+#   (optional) The endpoint's internal url. (Defaults to 'http://127.0.0.1:8082
+#   This url should *not* contain any trailing '/'.
+#
+# === Examples
+#
+#  class { 'murano::keystone::auth':
+#    public_url   => 'https://10.0.0.10:8082',
+#    internal_url => 'https://10.0.0.11:8082',
+#    admin_url    => 'https://10.0.0.11:8082',
+#  }
+#
+class murano::keystone::auth(
+  $password,
+  $service_name        = undef,
+  $auth_name           = 'murano',
+  $email               = 'murano@localhost',
+  $tenant              = 'services',
+  $service_type        = 'application_catalog',
+  $service_description = 'Murano Application Catalog',
+  $configure_endpoint  = true,
+  $region              = 'RegionOne',
+  $public_url          = 'http://127.0.0.1:8082',
+  $admin_url           = 'http://127.0.0.1:8082',
+  $internal_url        = 'http://127.0.0.1:8082',
+) {
+
+  $real_service_name = pick($service_name, $auth_name)
+
+  keystone::resource::service_identity { $real_service_name:
+    configure_user      => true,
+    configure_user_role => true,
+    configure_endpoint  => $configure_endpoint,
+    service_type        => $service_type,
+    service_description => $service_description,
+    region              => $region,
+    password            => $password,
+    email               => $email,
+    tenant              => $tenant,
+    public_url          => $public_url,
+    admin_url           => $admin_url,
+    internal_url        => $internal_url,
+  }
+}
---
+++ spec/classes/murano_keystone_auth_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+describe 'murano::keystone::auth' do
+
+  let :facts do
+    { :osfamily => 'Debian' }
+  end
+
+  describe 'with default class parameters' do
+    let :params do
+      { :password => 'murano_password',
+        :tenant   => 'foobar' }
+    end
+
+    it { is_expected.to contain_keystone_user('murano').with(
+                            :ensure   => 'present',
+                            :password => 'murano_password',
+                            :tenant   => 'foobar'
+                        ) }
+
+    it { is_expected.to contain_keystone_user_role('murano@foobar').with(
+                            :ensure  => 'present',
+                            :roles   => ['admin']
+                        )}
+
+    it { is_expected.to contain_keystone_service('murano').with(
+                            :ensure      => 'present',
+                            :type        => 'application_catalog',
+                            :description => 'Murano Application Catalog'
+                        ) }
+
+    it { is_expected.to contain_keystone_endpoint('RegionOne/murano').with(
+                            :ensure       => 'present',
+                            :public_url   => "http://127.0.0.1:8082",
+                            :admin_url    => "http://127.0.0.1:8082",
+                            :internal_url => "http://127.0.0.1:8082"
+                        ) }
+  end
+
+  describe 'with endpoint parameters' do
+    let :params do
+      { :password     => 'murano_password',
+        :public_url   => 'https://10.10.10.10:80',
+        :internal_url => 'http://10.10.10.11:81',
+        :admin_url    => 'http://10.10.10.12:81' }
+    end
+
+    it { is_expected.to contain_keystone_endpoint('RegionOne/murano').with(
+                            :ensure       => 'present',
+                            :public_url   => 'https://10.10.10.10:80',
+                            :internal_url => 'http://10.10.10.11:81',
+                            :admin_url    => 'http://10.10.10.12:81'
+                        ) }
+  end
+
+  describe 'when overriding auth name' do
+    let :params do
+      { :password => 'foo',
+        :auth_name => 'muranoy' }
+    end
+
+    it { is_expected.to contain_keystone_user('muranoy') }
+    it { is_expected.to contain_keystone_user_role('muranoy@services') }
+    it { is_expected.to contain_keystone_service('muranoy') }
+    it { is_expected.to contain_keystone_endpoint('RegionOne/muranoy') }
+  end
+end
