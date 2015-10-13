172737af005a14c38d16eff906090557fb5f4e82
Matt Fischer <matt@mattfischer.com>
2015-10-12 20:12:54 -0600

    /etc/murano/murano.conf created before configuring
    Ensure that the common package is installed before attempting any
    configuration. Without this you have resource ordering issues.
    
    Change-Id: I30c8a9d1f9458ff653ca0e442bca33ab30bf74f7

--- spec/unit/type/murano_config_spec.rb
+++ spec/unit/type/murano_config_spec.rb
@@ -49,4 +49,14 @@ describe 'Puppet::Type.type(:murano_config)' do
       @murano_config[:ensure] = :latest
     }.to raise_error(Puppet::Error, /Invalid value/)
   end
+
+  it 'should autorequire the package that install the file' do
+    catalog = Puppet::Resource::Catalog.new
+    package = Puppet::Type.type(:package).new(:name => 'murano-common')
+    catalog.add_resource package, @murano_config
+    dependency = @murano_config.autorequire
+    expect(dependency.size).to eq(1)
+    expect(dependency[0].target).to eq(@murano_config)
+    expect(dependency[0].source).to eq(package)
+  end
 end
