74b40b17ee9461d63b5aea73ba84f34130b97fe8
ZhongShengping <chdzsp@163.com>
2016-06-04 00:25:55 +0800

    Test multiple operating systems for policy
    Use on_supported_os function to test multiple
    operating systems for class murano::policy.
    
    Change-Id: I801b3f247dd6649c2e7794f29b7c35f45d0c724c

--- spec/classes/murano_policy_spec.rb
+++ spec/classes/murano_policy_spec.rb
@@ -24,19 +24,16 @@ describe 'murano::policy' do
     end
   end
 
-  context 'on Debian platforms' do
-    let :facts do
-      { :osfamily => 'Debian' }
-    end
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge(OSDefaults.get_facts({:processorcount => 8}))
+      end
 
-    it_configures 'murano policies'
+      it_configures 'murano policies'
+    end
   end
 
-  context 'on RedHat platforms' do
-    let :facts do
-      { :osfamily => 'RedHat' }
-    end
-
-    it_configures 'murano policies'
-  end
 end
