99fadd681e083a02a12bd0e08f9ad811c95466f3
Sharat Sharma <sharat.sharma@nectechnologies.in>
2016-09-29 12:38:35 +0530

    Added retries for db_sync
    If db sync fails, it is never retried leading to errors. So, this
    patch adds retries for db_sync to avoid sync fails.
    
    Change-Id: I1ee9f046284d8720447c02f0fbbbc4448eaaf4d9
    Related-Bug: #1628580
    Closes-Bug: #1645474
    (cherry picked from commit e7db0e93f3edeb6d5857fbd5149a89a9b10f63a2)

--- manifests/db/sync.pp
+++ manifests/db/sync.pp
@@ -15,6 +15,8 @@ class murano::db::sync {
     path        => '/usr/bin',
     user        => 'murano',
     refreshonly => true,
+    try_sleep   => 5,
+    tries       => 10,
     logoutput   => on_failure,
   }
 
