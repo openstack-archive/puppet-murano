b3c62879a562670a845c8ec94417c7b3c0ac4cb4
Maksym Yatsenko <maxy.1982@gmail.com>
2016-10-10 18:55:15 +0300

    Remove '::processorcount' fact from tests.
    As '::processorcount' fact is not used anymore,
    it was removed from tests.
    
    Change-Id: Ifc6235bec254979f0d956d4b6dc985c0e0cfe4ae

--- spec/classes/murano_db_sync_spec.rb
+++ spec/classes/murano_db_sync_spec.rb
@@ -22,7 +22,6 @@ describe 'murano::db::sync' do
     context "on #{os}" do
       let (:facts) do
         facts.merge(OSDefaults.get_facts({
-          :processorcount => 8,
           :concat_basedir => '/var/lib/puppet/concat'
         }))
       end
