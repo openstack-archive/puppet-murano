8bfb89bcb8b272fb42cb8f9c26b886476dbf9d90
ZhongShengping <chdzsp@163.com>
2016-12-01 17:20:31 +0800

    Fix spec test for sync
    Change-Id: I8ddbe820096f22a931962ba7421acef73b187f3a

--- spec/classes/murano_db_sync_spec.rb
+++ spec/classes/murano_db_sync_spec.rb
@@ -10,6 +10,8 @@ describe 'murano::db::sync' do
         :path        => '/usr/bin',
         :user        => 'murano',
         :refreshonly => 'true',
+        :try_sleep   => 5,
+        :tries       => 10,
         :logoutput   => 'on_failure'
       )
     end
