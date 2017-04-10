6c278abde9258969653728f3bc4a10bc2c7c1ec4
ZhongShengping <chdzsp@163.com>
2017-04-10 19:53:52 +0800

    oslo db: check puppet resource instead of actual config in spec
    Change-Id: Iefab95934edea137a6b257eaa73e87841890e391

--- spec/classes/murano_db_spec.rb
+++ spec/classes/murano_db_spec.rb
@@ -5,14 +5,16 @@ describe 'murano::db' do
   shared_examples 'murano::db' do
 
     context 'with default parameters' do
-      it { is_expected.to contain_murano_config('database/connection').with_value('mysql://murano:secrete@localhost:3306/murano') }
-      it { is_expected.to contain_murano_config('database/idle_timeout').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_murano_config('database/min_pool_size').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_murano_config('database/max_retries').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_murano_config('database/retry_interval').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_murano_config('database/max_pool_size').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_murano_config('database/max_overflow').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_murano_config('database/db_max_retries').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_oslo__db('murano_config').with(
+        :db_max_retries => '<SERVICE DEFAULT>',
+        :connection     => 'mysql://murano:secrete@localhost:3306/murano',
+        :idle_timeout   => '<SERVICE DEFAULT>',
+        :min_pool_size  => '<SERVICE DEFAULT>',
+        :max_pool_size  => '<SERVICE DEFAULT>',
+        :max_retries    => '<SERVICE DEFAULT>',
+        :retry_interval => '<SERVICE DEFAULT>',
+        :max_overflow   => '<SERVICE DEFAULT>',
+      )}
     end
 
     context 'with specific parameters' do
@@ -28,19 +30,21 @@ describe 'murano::db' do
         }
       end
 
-      it { is_expected.to contain_murano_config('database/connection').with_value('mysql+pymysql://murano:murano@localhost/murano') }
-      it { is_expected.to contain_murano_config('database/idle_timeout').with_value('3601') }
-      it { is_expected.to contain_murano_config('database/min_pool_size').with_value('2') }
-      it { is_expected.to contain_murano_config('database/max_retries').with_value('11') }
-      it { is_expected.to contain_murano_config('database/retry_interval').with_value('11') }
-      it { is_expected.to contain_murano_config('database/max_pool_size').with_value('11') }
-      it { is_expected.to contain_murano_config('database/max_overflow').with_value('21') }
-      it { is_expected.to contain_murano_config('database/db_max_retries').with_value('-1') }
+      it { is_expected.to contain_oslo__db('murano_config').with(
+        :db_max_retries => '-1',
+        :connection     => 'mysql+pymysql://murano:murano@localhost/murano',
+        :idle_timeout   => '3601',
+        :min_pool_size  => '2',
+        :max_pool_size  => '11',
+        :max_retries    => '11',
+        :retry_interval => '11',
+        :max_overflow   => '21',
+      )}
     end
 
     context 'with postgresql backend' do
       let :params do
-        { :database_connection     => 'postgresql://murano:murano@localhost/murano', }
+        { :database_connection => 'postgresql://murano:murano@localhost/murano', }
       end
 
       it 'install the proper backend package' do
@@ -51,7 +55,7 @@ describe 'murano::db' do
 
     context 'with MySQL-python library as backend package' do
       let :params do
-        { :database_connection     => 'mysql://murano:murano@localhost/murano', }
+        { :database_connection => 'mysql://murano:murano@localhost/murano', }
       end
 
       it { is_expected.to contain_package('python-mysqldb').with(:ensure => 'present') }
@@ -59,7 +63,7 @@ describe 'murano::db' do
 
     context 'with incorrect database_connection string' do
       let :params do
-        { :database_connection     => 'sqlite://murano:murano@localhost/murano', }
+        { :database_connection => 'sqlite://murano:murano@localhost/murano', }
       end
 
       it_raises 'a Puppet::Error', /validate_re/
@@ -67,7 +71,7 @@ describe 'murano::db' do
 
     context 'with incorrect pymysql database_connection string' do
       let :params do
-        { :database_connection     => 'foo+pymysql://murano:murano@localhost/murano', }
+        { :database_connection => 'foo+pymysql://murano:murano@localhost/murano', }
       end
 
       it_raises 'a Puppet::Error', /validate_re/
@@ -86,7 +90,7 @@ on_supported_os({
 
       context 'using pymysql driver' do
         let :params do
-          { :database_connection     => 'mysql+pymysql://murano:murano@localhost/murano' }
+          { :database_connection => 'mysql+pymysql://murano:murano@localhost/murano' }
         end
 
         case facts[:osfamily]
