c4eec07f1ce20c478e13b162963b9591c964549e
ZhongShengping <chdzsp@163.com>
2017-11-28 09:34:05 +0800

    Expose use_json logging option
    It enables JSON-formatted logging from oslo.log.
    
    Change-Id: I734df852f2efe4cc8b4098b906900ccdd23f086e

--- spec/classes/murano_logging_spec.rb
+++ spec/classes/murano_logging_spec.rb
@@ -11,6 +11,7 @@ describe 'murano::logging' do
     {
         :debug                         => 'true',
         :use_syslog                    => 'true',
+        :use_json                      => 'true',
         :use_stderr                    => 'false',
         :log_facility                  => 'LOG_LOCAL0',
         :log_dir                       => '/tmp/murano',
@@ -62,6 +63,7 @@ describe 'murano::logging' do
     context 'with defaults' do
      it { is_expected.to contain_oslo__log('murano_config').with(
         :use_syslog => '<SERVICE DEFAULT>',
+        :use_json   => '<SERVICE DEFAULT>',
         :use_stderr => '<SERVICE DEFAULT>',
         :log_dir    => '/var/log/murano',
         :debug      => '<SERVICE DEFAULT>',
@@ -84,6 +86,7 @@ describe 'murano::logging' do
     context 'with passed params' do
       it { is_expected.to contain_oslo__log('murano_config').with(
         :use_syslog          => true,
+        :use_json            => true,
         :use_stderr          => false,
         :syslog_log_facility => 'LOG_LOCAL0',
         :log_dir             => '/tmp/murano',
