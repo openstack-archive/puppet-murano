28e745c40f48622de9df1ba0f903eb2da54d72cd
Tobias Urdin <tobias.urdin@binero.se>
2018-10-30 15:52:16 +0100

    Use puppet4 functions-api
    Change-Id: I0877c6b94787ee2dd5c52c3b5e7ee2d525f660f0

--- spec/functions/get_ext_net_name_spec.rb
+++ spec/functions/get_ext_net_name_spec.rb
@@ -1,85 +1,63 @@
 require 'spec_helper'
 
 describe 'get_ext_net_name' do
-  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+  it 'exists' do
+    is_expected.not_to eq(nil)
+  end
 
-  it 'should exist' do
-    expect(Puppet::Parser::Functions.function('get_ext_net_name')).to eq('function_get_ext_net_name')
+  it 'fails with no arguments' do
+    is_expected.to run.with_params.and_raise_error(Puppet::ParseError)
   end
 
   it 'should return the network name that has router_ext enabled' do
-    expect(scope.function_get_ext_net_name(
-      [
-        {
-          "net04" =>
-          {
-            "L2" =>
-            {
-              "router_ext"   => false,
-            }
-          },
-          "net04_ext" =>
-          {
-            "L2" =>
-            {
-              "router_ext"   => true,
-            }
-          }
+    param = {
+      "net04" => {
+        "L2" => {
+          "router_ext" => false,
         }
-      ]
-    )).to eq 'net04_ext'
+      },
+      "net04_ext" => {
+        "L2" => {
+          "router_ext" => true,
+        }
+      }
+    }
+
+    is_expected.to run.with_params(param).and_return('net04_ext')
   end
 
   it 'should return nil if router_ext is not enabled' do
-    expect(scope.function_get_ext_net_name(
-      [
-        {
-          "net04" =>
-          {
-            "L2" =>
-            {
-              "router_ext"   => false,
-            }
-          },
-          "net04_ext" =>
-          {
-            "L2" =>
-            {
-              "router_ext"   => false,
-            }
-          }
+    param = {
+      "net04" => {
+        "L2" => {
+          "router_ext" => false,
         }
-      ],
-    )).to be_nil
+      },
+      "net04_ext" => {
+        "L2" => {
+          "router_ext" => false,
+        }
+      }
+    }
+
+    is_expected.to run.with_params(param).and_return(nil)
   end
 
   it 'should return nil if there is no router_ext' do
-    expect(scope.function_get_ext_net_name(
-      [
-        {
-          "net04" =>
-          {
-            "L2" =>
-            {
-            }
-          },
-          "net04_ext" =>
-          {
-            "L2" =>
-            {
-            }
-          }
-        }
-      ]
-    )).to be_nil
+    param = {
+      "net04" => {
+        "L2" => {}
+      },
+      "net04_ext" => {
+        "L2" => {}
+      }
+    }
+
+    is_expected.to run.with_params(param).and_return(nil)
   end
 
   it 'should return nil with empty network data' do
-    expect(scope.function_get_ext_net_name(
-      [
-        {}
-      ]
-    )).to be_nil
+    param = {}
+    is_expected.to run.with_params(param).and_return(nil)
   end
-
 end
