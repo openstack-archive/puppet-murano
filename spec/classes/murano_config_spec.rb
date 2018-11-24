2d494426afbacd75237cc0b229e6faa5dedb347f
Tobias Urdin <tobias.urdin@binero.se>
2018-11-24 15:35:24 +0100

    Convert to rspec-puppet-facts
    Change-Id: I495a0ac6cad5f7b251101059673b9cc4e5815d45

--- spec/classes/murano_config_spec.rb
+++ spec/classes/murano_config_spec.rb
@@ -1,47 +1,60 @@
 require 'spec_helper'
 
 describe 'murano::config' do
-
-  let :base_config do
-    {
+  shared_examples 'murano::config' do
+    let :base_config do
+      {
         'DEFAULT/foo' => { 'value'  => 'fooValue' },
         'DEFAULT/bar' => { 'value'  => 'barValue' },
         'DEFAULT/baz' => { 'ensure' => 'absent' }
-    }
+      }
+    end
+
+    let :params do
+      {
+        :murano_config             => base_config,
+        :murano_cfapi_config       => base_config,
+        :murano_paste_config       => base_config,
+        :murano_cfapi_paste_config => base_config
+      }
+    end
+
+    it { should contain_class('murano::deps') }
+
+    it 'configures arbitrary murano configurations' do
+      should contain_murano_config('DEFAULT/foo').with_value('fooValue')
+      should contain_murano_config('DEFAULT/bar').with_value('barValue')
+      should contain_murano_config('DEFAULT/baz').with_ensure('absent')
+    end
+
+    it 'configures murano cfapi configurations' do
+      should contain_murano_cfapi_config('DEFAULT/foo').with_value('fooValue')
+      should contain_murano_cfapi_config('DEFAULT/bar').with_value('barValue')
+      should contain_murano_cfapi_config('DEFAULT/baz').with_ensure('absent')
+    end
+
+    it 'configures arbitrary murano paste configurations' do
+      should contain_murano_paste_ini_config('DEFAULT/foo').with_value('fooValue')
+      should contain_murano_paste_ini_config('DEFAULT/bar').with_value('barValue')
+      should contain_murano_paste_ini_config('DEFAULT/baz').with_ensure('absent')
+    end
+
+    it 'configures murano cfapi paste configurations' do
+      should contain_murano_cfapi_paste_ini_config('DEFAULT/foo').with_value('fooValue')
+      should contain_murano_cfapi_paste_ini_config('DEFAULT/bar').with_value('barValue')
+      should contain_murano_cfapi_paste_ini_config('DEFAULT/baz').with_ensure('absent')
+    end
   end
 
-  let :params do
-    { :murano_config             => base_config,
-      :murano_cfapi_config       => base_config,
-      :murano_paste_config       => base_config,
-      :murano_cfapi_paste_config => base_config
-    }
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_behaves_like 'murano::config'
+    end
   end
-
-  it { is_expected.to contain_class('murano::deps') }
-
-  it 'configures arbitrary murano configurations' do
-    is_expected.to contain_murano_config('DEFAULT/foo').with_value('fooValue')
-    is_expected.to contain_murano_config('DEFAULT/bar').with_value('barValue')
-    is_expected.to contain_murano_config('DEFAULT/baz').with_ensure('absent')
-  end
-
-  it 'configures murano cfapi configurations' do
-    is_expected.to contain_murano_cfapi_config('DEFAULT/foo').with_value('fooValue')
-    is_expected.to contain_murano_cfapi_config('DEFAULT/bar').with_value('barValue')
-    is_expected.to contain_murano_cfapi_config('DEFAULT/baz').with_ensure('absent')
-  end
-
-  it 'configures arbitrary murano paste configurations' do
-    is_expected.to contain_murano_paste_ini_config('DEFAULT/foo').with_value('fooValue')
-    is_expected.to contain_murano_paste_ini_config('DEFAULT/bar').with_value('barValue')
-    is_expected.to contain_murano_paste_ini_config('DEFAULT/baz').with_ensure('absent')
-  end
-
-  it 'configures murano cfapi paste configurations' do
-    is_expected.to contain_murano_cfapi_paste_ini_config('DEFAULT/foo').with_value('fooValue')
-    is_expected.to contain_murano_cfapi_paste_ini_config('DEFAULT/bar').with_value('barValue')
-    is_expected.to contain_murano_cfapi_paste_ini_config('DEFAULT/baz').with_ensure('absent')
-  end
-
 end
--- spec/classes/murano_dashboard_spec.rb
+++ spec/classes/murano_dashboard_spec.rb
@@ -1,39 +1,36 @@
 require 'spec_helper'
 
 describe 'murano::dashboard' do
-
   let :params do
+    {}
+  end
+
+  let :dashboard_params do
     {
+      :dashboard_name          => 'Application Catalog',
+      :repo_url                => 'http://storage.apps.openstack.com',
+      :enable_glare            => true,
+      :collect_static_script   => '/bin/openstack-dashboard/manage.py',
+      :metadata_dir            => '/tmp/muranodashboard-cache',
+      :max_file_size           => '5',
+      :sync_db                 => false,
+      :log_handler             => 'console',
+      :image_filter_project_id => '00000000-0000-0000-0000-000000000000',
     }
-  end
+    end
 
-  let :dashboard_params do {
-    :dashboard_name          => 'Application Catalog',
-    :repo_url                => 'http://storage.apps.openstack.com',
-    :enable_glare            => true,
-    :collect_static_script   => '/bin/openstack-dashboard/manage.py',
-    :metadata_dir            => '/tmp/muranodashboard-cache',
-    :max_file_size           => '5',
-    :sync_db                 => false,
-    :log_handler             => 'console',
-    :image_filter_project_id => '00000000-0000-0000-0000-000000000000',
-  }
-  end
-
-  shared_examples_for 'murano dashboard' do
+  shared_examples 'murano::dashboard' do
     context 'with basic dashboard options and default settings' do
-      it_configures 'with default class parameters'
+      it_behaves_like 'with default class parameters'
     end
 
     context 'with non-default settings' do
       before { params.merge!( dashboard_params ) }
-      it_configures 'with parameters override'
+      it_behaves_like 'with parameters override'
     end
   end
 
   shared_examples 'with default class parameters' do
-
-
     let(:collect_static_command) {
       if facts[:os_package_type] == 'ubuntu'
         "/usr/share/openstack-dashboard/manage.py collectstatic --noinput"
@@ -42,53 +39,52 @@ describe 'murano::dashboard' do
       end
     }
 
-    it { is_expected.to contain_package('murano-dashboard').with({
+    it { should contain_package('murano-dashboard').with({
       :ensure => 'present',
-      :name   => 'python-murano-dashboard',
+      :name   => platform_params[:dashboard_package_name],
     })}
 
-    it { is_expected.to contain_concat('/etc/openstack-dashboard/local_settings.py')}
-    it { is_expected.to contain_concat__fragment('original_config').with({
-      :target => '/etc/openstack-dashboard/local_settings.py',
-      :source => '/etc/openstack-dashboard/local_settings.py',
+    it { should contain_concat(platform_params[:dashboard_config])}
+    it { should contain_concat__fragment('original_config').with({
+      :target => platform_params[:dashboard_config],
+      :source => platform_params[:dashboard_config],
       :order  => 1,
     })}
 
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with({
-      :target => '/etc/openstack-dashboard/local_settings.py',
+    it { should contain_concat__fragment('murano_dashboard_section').with({
+      :target => platform_params[:dashboard_config],
       :order  => 2,
     })}
 
-    it { is_expected.to_not contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_API_URL = /)}
-    it { is_expected.to_not contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_REPO_URL = /)}
-    it { is_expected.to_not contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_DASHBOARD_NAME = /)}
-    it { is_expected.to_not contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_IMAGE_FILTER_PROJECT_ID = /)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/MAX_FILE_SIZE_MB = '5'/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/METADATA_CACHE_DIR = '\/var\/cache\/murano-dashboard'/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/LOGGING\['loggers'\]\['muranodashboard'\] = \{'handlers': 'file', 'level': 'DEBUG'\}/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/LOGGING\['loggers'\]\['muranoclient'\] = \{'handlers': 'file', 'level': 'ERROR'\}/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_USE_GLARE = False/)}
+    it { should_not contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_API_URL = /)}
+    it { should_not contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_REPO_URL = /)}
+    it { should_not contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_DASHBOARD_NAME = /)}
+    it { should_not contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_IMAGE_FILTER_PROJECT_ID = /)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/MAX_FILE_SIZE_MB = '5'/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/METADATA_CACHE_DIR = '\/var\/cache\/murano-dashboard'/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/LOGGING\['loggers'\]\['muranodashboard'\] = \{'handlers': 'file', 'level': 'DEBUG'\}/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/LOGGING\['loggers'\]\['muranoclient'\] = \{'handlers': 'file', 'level': 'ERROR'\}/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_USE_GLARE = False/)}
 
-    it { is_expected.to contain_exec('clean_horizon_config').with({
-      :command => 'sed -e \'/^## MURANO_CONFIG_BEGIN/,/^## MURANO_CONFIG_END ##/ d\' -i /etc/openstack-dashboard/local_settings.py',
-      :onlyif  => 'grep \'^## MURANO_CONFIG_BEGIN\' /etc/openstack-dashboard/local_settings.py',
+    it { should contain_exec('clean_horizon_config').with({
+      :command => "sed -e \'/^## MURANO_CONFIG_BEGIN/,/^## MURANO_CONFIG_END ##/ d\' -i #{platform_params[:dashboard_config]}",
+      :onlyif  => "grep \'^## MURANO_CONFIG_BEGIN\' #{platform_params[:dashboard_config]}",
     })}
 
-    it { is_expected.to contain_exec('django_collectstatic').with({
+    it { should contain_exec('django_collectstatic').with({
       :command => collect_static_command
     })}
 
-    it { is_expected.to contain_exec('django_compressstatic').with({
+    it { should contain_exec('django_compressstatic').with({
       :command => '/usr/share/openstack-dashboard/manage.py compress --force'
     })}
 
-    it { is_expected.to contain_exec('django_syncdb').with({
+    it { should contain_exec('django_syncdb').with({
       :command => '/usr/share/openstack-dashboard/manage.py migrate --noinput'
     })}
   end
 
   shared_examples 'with parameters override' do
-
     let(:collect_static_command) {
       if facts[:os_package_type] == 'ubuntu'
         "/bin/openstack-dashboard/manage.py collectstatic --noinput"
@@ -97,59 +93,76 @@ describe 'murano::dashboard' do
       end
     }
 
-    it { is_expected.to contain_package('murano-dashboard').with({
+    it { should contain_package('murano-dashboard').with({
       :ensure => 'present',
-      :name   => 'python-murano-dashboard',
+      :name   => platform_params[:dashboard_package_name],
     })}
 
-    it { is_expected.to contain_concat('/etc/openstack-dashboard/local_settings.py')}
-    it { is_expected.to contain_concat__fragment('original_config').with({
-      :target => '/etc/openstack-dashboard/local_settings.py',
-      :source => '/etc/openstack-dashboard/local_settings.py',
+    it { should contain_concat(platform_params[:dashboard_config])}
+    it { should contain_concat__fragment('original_config').with({
+      :target => platform_params[:dashboard_config],
+      :source => platform_params[:dashboard_config],
       :order  => 1,
     })}
 
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with({
-      :target => '/etc/openstack-dashboard/local_settings.py',
+    it { should contain_concat__fragment('murano_dashboard_section').with({
+      :target => platform_params[:dashboard_config],
       :order  => 2,
     })}
 
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_DASHBOARD_NAME = 'Application Catalog'/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_REPO_URL = 'http:\/\/storage.apps.openstack.com'/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/MAX_FILE_SIZE_MB = '5'/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/METADATA_CACHE_DIR = '\/tmp\/muranodashboard-cache'/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/LOGGING\['loggers'\]\['muranodashboard'\] = \{'handlers': 'console', 'level': 'DEBUG'\}/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/LOGGING\['loggers'\]\['muranoclient'\] = \{'handlers': 'console', 'level': 'ERROR'\}/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_USE_GLARE = True/)}
-    it { is_expected.to contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_IMAGE_FILTER_PROJECT_ID = '00000000-0000-0000-0000-000000000000'/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_DASHBOARD_NAME = 'Application Catalog'/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_REPO_URL = 'http:\/\/storage.apps.openstack.com'/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/MAX_FILE_SIZE_MB = '5'/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/METADATA_CACHE_DIR = '\/tmp\/muranodashboard-cache'/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/LOGGING\['loggers'\]\['muranodashboard'\] = \{'handlers': 'console', 'level': 'DEBUG'\}/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/LOGGING\['loggers'\]\['muranoclient'\] = \{'handlers': 'console', 'level': 'ERROR'\}/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_USE_GLARE = True/)}
+    it { should contain_concat__fragment('murano_dashboard_section').with_content(/MURANO_IMAGE_FILTER_PROJECT_ID = '00000000-0000-0000-0000-000000000000'/)}
 
-    it { is_expected.to contain_exec('clean_horizon_config').with({
-      :command => 'sed -e \'/^## MURANO_CONFIG_BEGIN/,/^## MURANO_CONFIG_END ##/ d\' -i /etc/openstack-dashboard/local_settings.py',
-      :onlyif  => 'grep \'^## MURANO_CONFIG_BEGIN\' /etc/openstack-dashboard/local_settings.py',
+    it { should contain_exec('clean_horizon_config').with({
+      :command => "sed -e \'/^## MURANO_CONFIG_BEGIN/,/^## MURANO_CONFIG_END ##/ d\' -i #{platform_params[:dashboard_config]}",
+      :onlyif  => "grep \'^## MURANO_CONFIG_BEGIN\' #{platform_params[:dashboard_config]}",
     })}
 
-    it { is_expected.to contain_exec('django_collectstatic').with({
+    it { should contain_exec('django_collectstatic').with({
       :command => collect_static_command
     })}
 
-    it { is_expected.to contain_exec('django_compressstatic').with({
+    it { should contain_exec('django_compressstatic').with({
       :command => '/bin/openstack-dashboard/manage.py compress --force'
     })}
 
-    it { is_expected.to_not contain_exec('django_syncdb').with({
+    it { should_not contain_exec('django_syncdb').with({
       :command => '/bin/openstack-dashboard/manage.py syncdb --noinput'
     })}
   end
 
-  context 'on Debian platforms' do
-    let :facts do
-      OSDefaults.get_facts({
-        :osfamily        => 'Debian',
-        :operatingsystem => 'Debian',
-        :concat_basedir  => '/var/lib/puppet/concat',
-      })
-    end
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts({
+          :concat_basedir  => '/var/lib/puppet/concat',
+        }))
+      end
 
-    it_behaves_like 'murano dashboard'
+      let (:platform_params) do
+        case facts[:osfamily]
+        when 'Debian'
+          {
+            :dashboard_package_name => 'python-murano-dashboard',
+            :dashboard_config       => '/etc/openstack-dashboard/local_settings.py'
+          }
+        when 'RedHat'
+          {
+            :dashboard_package_name => 'openstack-murano-ui',
+            :dashboard_config       => '/etc/openstack-dashboard/local_settings'
+          }
+        end
+      end
+
+      it_behaves_like 'murano::dashboard'
+    end
   end
 end
