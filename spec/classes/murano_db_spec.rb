a8a1403e6d9b203f7a955ee284ce20bb30f8a979
Tobias Urdin <tobias.urdin@binero.se>
2019-01-18 10:45:26 +0100

    Remove redundantly tested code
    Remove code that is redundantly tested.
    
    This should not be tested here but in puppet-oslo
    where this logic resides.
    
    If we keep this and we do changes in puppet-oslo we
    will break these unit tests, this is something we need
    to sort out for all modules.
    
    Change-Id: I7c2ce8e6f69a418ed526565ac5b6656ac92eee4f

--- spec/classes/murano_db_spec.rb
+++ spec/classes/murano_db_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
 describe 'murano::db' do
-
   shared_examples 'murano::db' do
-
     context 'with default parameters' do
-      it { is_expected.to contain_oslo__db('murano_config').with(
+      it { should contain_oslo__db('murano_config').with(
         :db_max_retries => '<SERVICE DEFAULT>',
         :connection     => 'mysql+pymysql://murano:secrete@localhost:3306/murano',
         :idle_timeout   => '<SERVICE DEFAULT>',
@@ -20,7 +18,8 @@ describe 'murano::db' do
 
     context 'with specific parameters' do
       let :params do
-        { :database_connection       => 'mysql+pymysql://murano:murano@localhost/murano',
+        {
+          :database_connection       => 'mysql+pymysql://murano:murano@localhost/murano',
           :database_idle_timeout     => '3601',
           :database_min_pool_size    => '2',
           :database_max_retries      => '11',
@@ -32,7 +31,7 @@ describe 'murano::db' do
         }
       end
 
-      it { is_expected.to contain_oslo__db('murano_config').with(
+      it { should contain_oslo__db('murano_config').with(
         :db_max_retries => '-1',
         :connection     => 'mysql+pymysql://murano:murano@localhost/murano',
         :idle_timeout   => '3601',
@@ -44,45 +43,10 @@ describe 'murano::db' do
         :pool_timeout   => '21',
       )}
     end
-
-    context 'with postgresql backend' do
-      let :params do
-        { :database_connection => 'postgresql://murano:murano@localhost/murano', }
-      end
-
-      it 'install the proper backend package' do
-        is_expected.to contain_package('python-psycopg2').with(:ensure => 'present')
-      end
-
-    end
-
-    context 'with MySQL-python library as backend package' do
-      let :params do
-        { :database_connection => 'mysql+pymysql://murano:murano@localhost/murano', }
-      end
-
-      it { is_expected.to contain_package('python-mysqldb').with(:ensure => 'present') }
-    end
-
-    context 'with incorrect database_connection string' do
-      let :params do
-        { :database_connection => 'sqlite://murano:murano@localhost/murano', }
-      end
-
-      it_raises 'a Puppet::Error', /validate_re/
-    end
-
-    context 'with incorrect pymysql database_connection string' do
-      let :params do
-        { :database_connection => 'foo+pymysql://murano:murano@localhost/murano', }
-      end
-
-      it_raises 'a Puppet::Error', /validate_re/
-    end
   end
 
-on_supported_os({
-    :supported_os   => OSDefaults.get_supported_os
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
@@ -90,25 +54,6 @@ on_supported_os({
       end
 
       it_behaves_like 'murano::db'
-
-      context 'using pymysql driver' do
-        let :params do
-          { :database_connection => 'mysql+pymysql://murano:murano@localhost/murano' }
-        end
-
-        case facts[:osfamily]
-        when 'Debian'
-          it { is_expected.to contain_package('python-pymysql').with(
-            :ensure => 'present',
-            :name   => 'python-pymysql',
-            :tag    => 'openstack'
-          )}
-        when 'RedHat'
-          it { is_expected.not_to contain_package('python-pymysql') }
-        end
-      end
-
     end
   end
-
 end
