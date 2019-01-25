7f5da7c9674fb4bd0642c4013116336c574b2d29
ZhongShengping <chdzsp@163.com>
2019-01-25 14:41:28 +0800

    Fix unit test failed
    see:http://logs.openstack.org/11/633111/1/check/puppet-openstack-unit-5.5-centos-7/e720feb/job-output.txt.gz#_2019-01-25_05_51_56_212291
    
    Closes-Bug: #1813250
    
    Change-Id: I7906b16e175d9935dd3d39759f97394e5f6e3cf2

--- spec/classes/murano_db_cfapi_spec.rb
+++ spec/classes/murano_db_cfapi_spec.rb
@@ -28,50 +28,18 @@ describe 'murano::db_cfapi' do
         }
       end
 
-      it { is_expected.to contain_murano_cfapi_config('database/connection').with_value('mysql+pymysql://murano_cfapi:murano_cfapi@localhost/murano_cfapi') }
-      it { is_expected.to contain_murano_cfapi_config('database/idle_timeout').with_value('3601') }
-      it { is_expected.to contain_murano_cfapi_config('database/min_pool_size').with_value('2') }
-      it { is_expected.to contain_murano_cfapi_config('database/max_retries').with_value('11') }
-      it { is_expected.to contain_murano_cfapi_config('database/retry_interval').with_value('11') }
-      it { is_expected.to contain_murano_cfapi_config('database/max_pool_size').with_value('11') }
-      it { is_expected.to contain_murano_cfapi_config('database/max_overflow').with_value('21') }
-      it { is_expected.to contain_murano_cfapi_config('database/db_max_retries').with_value('-1') }
+      it { should contain_oslo__db('murano_cfapi_config').with(
+        :connection     => 'mysql+pymysql://murano_cfapi:murano_cfapi@localhost/murano_cfapi',
+        :idle_timeout   => '3601',
+        :min_pool_size  => '2',
+        :max_pool_size  => '11',
+        :max_retries    => '11',
+        :retry_interval => '11',
+        :max_overflow   => '21',
+        :db_max_retries => '-1',
+      )}
     end
 
-    context 'with postgresql backend' do
-      let :params do
-        { :database_connection     => 'postgresql://murano_cfapi:murano_cfapi@localhost/murano_cfapi', }
-      end
-
-      it 'install the proper backend package' do
-        is_expected.to contain_package('python-psycopg2').with(:ensure => 'present')
-      end
-
-    end
-
-    context 'with MySQL-python library as backend package' do
-      let :params do
-        { :database_connection     => 'mysql://murano_cfapi:murano_cfapi@localhost/murano_cfapi', }
-      end
-
-      it { is_expected.to contain_package('python-mysqldb').with(:ensure => 'present') }
-    end
-
-    context 'with incorrect database_connection string' do
-      let :params do
-        { :database_connection     => 'sqlite://murano_cfapi:murano_cfapi@localhost/murano_cfapi', }
-      end
-
-      it_raises 'a Puppet::Error', /validate_re/
-    end
-
-    context 'with incorrect pymysql database_connection string' do
-      let :params do
-        { :database_connection     => 'foo+pymysql://murano_cfapi:murano_cfapi@localhost/murano_cfapi', }
-      end
-
-      it_raises 'a Puppet::Error', /validate_re/
-    end
   end
 
   on_supported_os({
@@ -83,24 +51,6 @@ describe 'murano::db_cfapi' do
       end
 
       it_behaves_like 'murano::db_cfapi'
-
-      context 'using pymysql driver' do
-        let :params do
-          { :database_connection     => 'mysql+pymysql://murano_cfapi:murano_cfapi@localhost/murano_cfapi' }
-        end
-
-        case facts[:osfamily]
-        when 'Debian'
-          it { is_expected.to contain_package('python-pymysql').with(
-            :ensure => 'present',
-            :name   => 'python-pymysql',
-            :tag    => 'openstack'
-          )}
-        when 'RedHat'
-          it { is_expected.not_to contain_package('python-pymysql') }
-        end
-      end
-
     end
   end
 
