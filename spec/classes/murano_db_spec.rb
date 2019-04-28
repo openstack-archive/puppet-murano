5c5f33ce3328fed8d17750189bd1501e6ea351c1
ZhongShengping <chdzsp@163.com>
2019-04-28 15:00:11 +0800

    Deprecate idle_timeout option
    The idle_timeout parameter is deprecated, use connection_recycle_time
    instead[1].
    
    [1]https://review.opendev.org/#/c/334182/
    
    Change-Id: If8648ce2798bc24cf20272bb2bf4685f06d5d7b3
    Depends-On: https://review.opendev.org/656106/
    Closes-Bug: #1826692

--- spec/classes/murano_db_spec.rb
+++ spec/classes/murano_db_spec.rb
@@ -4,43 +4,43 @@ describe 'murano::db' do
   shared_examples 'murano::db' do
     context 'with default parameters' do
       it { should contain_oslo__db('murano_config').with(
-        :db_max_retries => '<SERVICE DEFAULT>',
-        :connection     => 'mysql+pymysql://murano:secrete@localhost:3306/murano',
-        :idle_timeout   => '<SERVICE DEFAULT>',
-        :min_pool_size  => '<SERVICE DEFAULT>',
-        :max_pool_size  => '<SERVICE DEFAULT>',
-        :max_retries    => '<SERVICE DEFAULT>',
-        :retry_interval => '<SERVICE DEFAULT>',
-        :max_overflow   => '<SERVICE DEFAULT>',
-        :pool_timeout   => '<SERVICE DEFAULT>',
+        :db_max_retries          => '<SERVICE DEFAULT>',
+        :connection              => 'mysql+pymysql://murano:secrete@localhost:3306/murano',
+        :connection_recycle_time => '<SERVICE DEFAULT>',
+        :min_pool_size           => '<SERVICE DEFAULT>',
+        :max_pool_size           => '<SERVICE DEFAULT>',
+        :max_retries             => '<SERVICE DEFAULT>',
+        :retry_interval          => '<SERVICE DEFAULT>',
+        :max_overflow            => '<SERVICE DEFAULT>',
+        :pool_timeout            => '<SERVICE DEFAULT>',
       )}
     end
 
     context 'with specific parameters' do
       let :params do
         {
-          :database_connection       => 'mysql+pymysql://murano:murano@localhost/murano',
-          :database_idle_timeout     => '3601',
-          :database_min_pool_size    => '2',
-          :database_max_retries      => '11',
-          :database_retry_interval   => '11',
-          :database_max_pool_size    => '11',
-          :database_max_overflow     => '21',
-          :database_pool_timeout     => '21',
-          :database_db_max_retries   => '-1',
+          :database_connection              => 'mysql+pymysql://murano:murano@localhost/murano',
+          :database_connection_recycle_time => '3601',
+          :database_min_pool_size           => '2',
+          :database_max_retries             => '11',
+          :database_retry_interval          => '11',
+          :database_max_pool_size           => '11',
+          :database_max_overflow            => '21',
+          :database_pool_timeout            => '21',
+          :database_db_max_retries          => '-1',
         }
       end
 
       it { should contain_oslo__db('murano_config').with(
-        :db_max_retries => '-1',
-        :connection     => 'mysql+pymysql://murano:murano@localhost/murano',
-        :idle_timeout   => '3601',
-        :min_pool_size  => '2',
-        :max_pool_size  => '11',
-        :max_retries    => '11',
-        :retry_interval => '11',
-        :max_overflow   => '21',
-        :pool_timeout   => '21',
+        :db_max_retries          => '-1',
+        :connection              => 'mysql+pymysql://murano:murano@localhost/murano',
+        :connection_recycle_time => '3601',
+        :min_pool_size           => '2',
+        :max_pool_size           => '11',
+        :max_retries             => '11',
+        :retry_interval          => '11',
+        :max_overflow            => '21',
+        :pool_timeout            => '21',
       )}
     end
   end
