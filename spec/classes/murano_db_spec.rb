115e75b20db5fac71da3b09ce76a060e0046ee15
ZhongShengping <chdzsp@163.com>
2020-08-26 11:53:17 +0800

    Add mysql_enable_ndb option
    Add mysql_enable_ndb parameter to select mysql storage engine.
    
    Change-Id: I4783acd3b0ec057d6fe58aff021505818417510b
    Depends-On: https://review.opendev.org/#/c/748067
    Closes-Bug: #1892952

--- spec/classes/murano_db_spec.rb
+++ spec/classes/murano_db_spec.rb
@@ -12,6 +12,7 @@ describe 'murano::db' do
         :retry_interval          => '<SERVICE DEFAULT>',
         :max_overflow            => '<SERVICE DEFAULT>',
         :pool_timeout            => '<SERVICE DEFAULT>',
+        :mysql_enable_ndb        => '<SERVICE DEFAULT>',
       )}
     end
 
@@ -25,6 +26,7 @@ describe 'murano::db' do
           :database_max_pool_size           => '11',
           :database_max_overflow            => '21',
           :database_pool_timeout            => '21',
+          :mysql_enable_ndb                 => true,
           :database_db_max_retries          => '-1',
         }
       end
@@ -38,6 +40,7 @@ describe 'murano::db' do
         :retry_interval          => '11',
         :max_overflow            => '21',
         :pool_timeout            => '21',
+        :mysql_enable_ndb        => true,
       )}
     end
   end
