9a2a82b4b081bfd0c61f4900f831e4006e2f8ecd
Thomas Goirand <zigo@debian.org>
2021-04-01 23:13:30 +0200

    Allow to configure policy_dirs
    This patch makes it possible to override the current service default,
    which is /etc/<service>/policy.d .
    
    Change-Id: Ideeb71d8fbdcffba89c1c98eebd9ca21f2403eee

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -32,11 +32,16 @@
 #   (Optional) Path to the murano policy.yaml file
 #   Defaults to /etc/murano/policy.yaml
 #
+# [*policy_dirs*]
+#   (Optional) Path to the murano policy folder
+#   Defaults to $::os_service_default
+#
 class murano::policy (
   $enforce_scope        = $::os_service_default,
   $enforce_new_defaults = $::os_service_default,
   $policies             = {},
   $policy_path          = '/etc/murano/policy.yaml',
+  $policy_dirs          = $::os_service_default,
 ) {
 
   include murano::deps
@@ -56,7 +61,8 @@ class murano::policy (
   oslo::policy { 'murano_config':
     enforce_scope        => $enforce_scope,
     enforce_new_defaults => $enforce_new_defaults,
-    policy_file          => $policy_path
+    policy_file          => $policy_path,
+    policy_dirs          => $policy_dirs,
   }
 
 }
