7cae4ca51c7749b50f731c2f5ede8f0d66c1300e
Takashi Kajinami <tkajinam@redhat.com>
2022-01-24 15:30:58 +0900

    Do not test detail of keystone::auth::service_identity
    ... but test interfaces of that resource type. This helps us avoid
    direct breakage caused by any change in puppet-keystone.
    
    Change-Id: I4f7bdca9d1d306c014110d3e017eb0e17aacecb4

--- spec/classes/murano_keystone_cfapi_auth_spec.rb
+++ spec/classes/murano_keystone_cfapi_auth_spec.rb
@@ -1,76 +1,80 @@
+#
+# Unit tests for murano::keystone::cfapi_auth
+#
+
 require 'spec_helper'
 
 describe 'murano::keystone::cfapi_auth' do
-
-  shared_examples_for 'murano keystone cfapi auth' do
-
-    describe 'with default class parameters' do
+  shared_examples_for 'murano::keystone::cfapi_auth' do
+    context 'with default class parameters' do
       let :params do
-        { :password => 'murano_password',
-          :tenant   => 'foobar' }
+        { :password => 'murano-cfapi_password' }
       end
 
-      it { is_expected.to contain_keystone_service('murano-cfapi::service-broker').with(
-                              :ensure      => 'present',
-                              :description => 'Murano Service Broker API'
-                          ) }
-
-      it { is_expected.to contain_keystone_endpoint('RegionOne/murano-cfapi::service-broker').with(
-                              :ensure       => 'present',
-                              :public_url   => "http://127.0.0.1:8083",
-                              :admin_url    => "http://127.0.0.1:8083",
-                              :internal_url => "http://127.0.0.1:8083"
-                          ) }
+      it { is_expected.to contain_keystone__resource__service_identity('murano-cfapi').with(
+        :configure_user      => false,
+        :configure_user_role => false,
+        :configure_endpoint  => true,
+        :service_name        => 'murano-cfapi',
+        :service_type        => 'service-broker',
+        :service_description => 'Murano Service Broker API',
+        :region              => 'RegionOne',
+        :auth_name           => 'murano-cfapi',
+        :password            => 'murano-cfapi_password',
+        :email               => 'murano@localhost',
+        :tenant              => 'services',
+        :public_url          => 'http://127.0.0.1:8083',
+        :internal_url        => 'http://127.0.0.1:8083',
+        :admin_url           => 'http://127.0.0.1:8083',
+      ) }
     end
 
-    describe 'with endpoint parameters' do
+    context 'when overriding parameters' do
       let :params do
-        { :password     => 'murano_password',
-          :public_url   => 'https://10.10.10.10:80',
-          :internal_url => 'http://10.10.10.11:81',
-          :admin_url    => 'http://10.10.10.12:81' }
+        { :password            => 'murano-cfapi_password',
+          :auth_name           => 'alt_murano-cfapi',
+          :email               => 'alt_murano@alt_localhost',
+          :tenant              => 'alt_service',
+          :configure_endpoint  => false,
+          :configure_user      => false,
+          :configure_user_role => false,
+          :service_description => 'Alternative Murano Service Broker API',
+          :service_name        => 'alt_service',
+          :service_type        => 'alt_service-broker',
+          :region              => 'RegionTwo',
+          :public_url          => 'https://10.10.10.10:80',
+          :internal_url        => 'http://10.10.10.11:81',
+          :admin_url           => 'http://10.10.10.12:81' }
       end
 
-      it { is_expected.to contain_keystone_endpoint('RegionOne/murano-cfapi::service-broker').with(
-                              :ensure       => 'present',
-                              :public_url   => 'https://10.10.10.10:80',
-                              :internal_url => 'http://10.10.10.11:81',
-                              :admin_url    => 'http://10.10.10.12:81'
-                          ) }
-    end
-
-    describe 'when overriding service name' do
-      let :params do
-        { :password => 'foo',
-          :service_name => 'murano-cfapiy' }
-      end
-
-      it { is_expected.to contain_keystone_service('murano-cfapiy::service-broker') }
-      it { is_expected.to contain_keystone_endpoint('RegionOne/murano-cfapiy::service-broker') }
-    end
-
-    describe 'when configuring user and role' do
-      let :params do
-        { :password => 'foo',
-          :configure_user => true,
-          :configure_user_role => true }
-      end
-
-      it { is_expected.to contain_keystone_user('murano-cfapi') }
-      it { is_expected.to contain_keystone_user_role('murano-cfapi@services') }
+      it { is_expected.to contain_keystone__resource__service_identity('murano-cfapi').with(
+        :configure_user      => false,
+        :configure_user_role => false,
+        :configure_endpoint  => false,
+        :service_name        => 'alt_service',
+        :service_type        => 'alt_service-broker',
+        :service_description => 'Alternative Murano Service Broker API',
+        :region              => 'RegionTwo',
+        :auth_name           => 'alt_murano-cfapi',
+        :password            => 'murano-cfapi_password',
+        :email               => 'alt_murano@alt_localhost',
+        :tenant              => 'alt_service',
+        :public_url          => 'https://10.10.10.10:80',
+        :internal_url        => 'http://10.10.10.11:81',
+        :admin_url           => 'http://10.10.10.12:81',
+      ) }
     end
   end
 
   on_supported_os({
-    :supported_os   => OSDefaults.get_supported_os
+    :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_behaves_like 'murano keystone cfapi auth'
+      it_behaves_like 'murano::keystone::cfapi_auth'
     end
   end
-
 end
