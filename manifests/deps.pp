0a53e1d4ac6296bbaab5704dc87d34db90bf54fa
ZhongShengping <chdzsp@163.com>
2022-11-09 08:54:46 +0800

    Fix dependencies related to openstacklib::policy
    Since [1] was merged, not only openstacklib::poliy::base but also
    openstacklib::policy::default is included to manage the policy file.
    This change ensure openstacklib::policy::default is executed after
    the packages are installed.
    
    [1] 740d1bb822e8543535a2fccb4f2eab4b5809422c
    
    Change-Id: I648ae1113f57f5cd085e8240af74a3c5750b58dc

--- manifests/deps.pp
+++ manifests/deps.pp
@@ -30,7 +30,7 @@ class murano::deps {
 
   # policy config should occur in the config block also.
   Anchor['murano::config::begin']
-  -> Openstacklib::Policy::Base<||>
+  -> Openstacklib::Policy<||>
   ~> Anchor['murano::config::end']
 
   # Installation or config changes will always restart services.
