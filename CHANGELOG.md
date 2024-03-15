04efdf7e6730784844363f7adbb49d631591358a
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-03-15 18:12:32 +0900

    Retire puppet-murano: Remove Project Content
    Depends-on: https://review.opendev.org/c/openstack/project-config/+/913292
    Change-Id: I594aeaafc4f4b40978b7a9c9676bd5d5ee6e373e

--- CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-## 8.0.0 and beyond
-
-From 8.0.0 release and beyond, release notes are published on
-[docs.openstack.org](http://docs.openstack.org/releasenotes/puppet-murano/).
-
-##2015-11-24 - 7.0.0
-###Summary
-
-- Initial release of the puppet-murano module
--- lib/puppet/functions/get_ext_net_name.rb
+++
@@ -1,9 +0,0 @@
-Puppet::Functions.create_function(:get_ext_net_name) do
-  def get_ext_net_name(*args)
-    networks = args.first
-    raise(Puppet::ParseError, 'get_ext_net_name(): No network data provided!') unless networks.is_a? Hash
-    ext_net_array = networks.find { |_, value| value.fetch('L2', {})['router_ext'] }
-    return nil unless ext_net_array
-    ext_net_array.first
-  end
-end
